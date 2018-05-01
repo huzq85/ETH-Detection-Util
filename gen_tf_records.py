9baa52f4b741e23ccaf64d976bf86dfe79fae272
Zhenqing Hu <huzq85@gmail.com>
2018-04-30 23:18:21 -0400

    Parsing the annotation file in order to retrieve the annotation information. This version mostly care about the parse of the annotation file. Image encoding and reading may not be supported in this version.
    Signed-off-by: Zhenqing Hu <huzq85@gmail.com>

--- gen_tf_records.py
+++ gen_tf_records.py
@@ -1,4 +1,5 @@
 import os
+import re
 import cv2
 import tensorflow as tf
 
@@ -12,20 +13,20 @@ FLAGS = flags.FLAGS
 
 def create_tf_example(example):
   # TODO(user): Populate the following variables from your example.
-  height = None # Image height
-  width = None # Image width
-  filename = None # Filename of the image. Empty if image is not from file
+  height = example[0] # Image height
+  width = example[1] # Image width
+  filename = example[2] # Filename of the image. Empty if image is not from file
   encoded_image_data = None # Encoded image bytes
-  image_format = None # b'jpeg' or b'png'
+  image_format = b'png' # b'jpeg' or b'png'
 
-  xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
-  xmaxs = [] # List of normalized right x coordinates in bounding box
+  xmins = example[3] # List of normalized left x coordinates in bounding box (1 per box)
+  xmaxs = example[4] # List of normalized right x coordinates in bounding box
              # (1 per box)
-  ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
-  ymaxs = [] # List of normalized bottom y coordinates in bounding box
+  ymins = example[5] # List of normalized top y coordinates in bounding box (1 per box)
+  ymaxs = example[6] # List of normalized bottom y coordinates in bounding box
              # (1 per box)
-  classes_text = ['Person'] # List of string class name of bounding box (1 per box)
-  classes = [-1] # List of integer class id of bounding box (1 per box)
+  classes_text = example[7] # List of string class name of bounding box (1 per box)
+  classes = example[8] # List of integer class id of bounding box (1 per box)
 
   tf_example = tf.train.Example(features=tf.train.Features(feature={
       'image/height': dataset_util.int64_feature(height),
@@ -43,36 +44,91 @@ def create_tf_example(example):
   }))
   return tf_example
 
-# Reading properties for every image file accompany with the annotation information
-def get_files(f_path):
-    # Get all of the images
-  for root, parent, files in os.walk(r'E:\5-DataSets\ETH\Setup1\BAHNHOF'):
-      for file in files:
-          if (file.endswith('png')):
-              img_file = os.path.join(root, file)
-              print('Reading: ' + img_file) # Get the image files
-              img = cv2.imread(img_file)
-              height = img.shape[0]
-              width = img.shape[1]
-          if (file.endswith('idl')):
-              annotation_file = os.path.join(root, file)
-              print(annotation_file) # Get the annotation file
-              f_read = open(annotation_file)
-              lines = f_read.readlines()
-              for line in lines:
-                  file_name_from_anno = line.split(':')[0] # "left/image_00000986.png"
-                  anno_info = line.split(':')[1:] # annotation info for every image
-                  
-                  
-                  
+# Reading and processing annotations
+def read_annotations(f_path):
+    anno_map = {}
+    for root, parent, files in os.walk(f_path):
+        for file in files:
+            if (file.endswith('idl')):
+                  annotation_file = os.path.join(root, file)
+                  print(annotation_file) # Get the annotation file
+                  f_read = open(annotation_file)
+                  lines = f_read.readlines()
+                  for line in lines:
+                      file_name_from_anno = line.split(':')[0] # "left/image_00000986.png"
+                      annos_info = line[line.index(':')+1:-2] # annotation info for every image, 
+                      # drop the last character ';' or '.' in every line
+                      # [' (212, 209, 238, 270)', '-1, (233, 201, 260, 284)', '-1, (287, 215, 305, 260)', '-1']
+                      annos_info = annos_info.lstrip()
+                      annos_info_list = re.compile("(?<!^)\s+(?=[,(])(?!.\s)").split(annos_info)
+                      anno_map[file_name_from_anno] = annos_info_list
+    print(res for res in anno_map) # This line used for debug
+    return anno_map
+            
+# Reading properties for every image
+def read_images(f_path):
+    imgs_map = {}
+    for root, parent, files in os.walk(f_path):
+        for file in files:
+            if (file.endswith('png')):
+                img_file = os.path.join(root, file)
+                print('Reading: ' + img_file)
+                img = cv2.imread(img_file)
+                 # height <-- img.shape[0]
+                 # width <-- img.shape[1]
+                imgs_map[file] = img.shape
+    print(res for res in imgs_map)
+    return imgs_map
+
+#  Making examples
+# [480, 640, '"left/image_00000003.png"', 450, 477, 212, 278, 'Person', '-1']
+def make_examples(f_path):
+    examples = []
+    imgs = read_images(f_path)
+    annos = read_annotations(f_path)
+    for anno_key in annos.keys():
+        # print(anno_key)
+        anno_key_sp = anno_key.split('/')[1]
+        anno_key_sp = anno_key_sp.split('.')[0]
+        # print(anno_key_sp)
+        for img_key in imgs.keys():
+            if anno_key_sp in img_key:
+                #print(annos[anno_key])
+                #print(imgs[img_key])
+                annos_list = annos[anno_key]
+                imgs_tuple = imgs[img_key]
+                print(annos_list)
+                print(imgs_tuple)
+                for single_anno in annos_list:
+                    single_instance = []
+                    # Process coordinates
+                    coordinate_str = single_anno.split(':')[0]
+                    coordinate_tuple = eval(coordinate_str)
+                    xmin = coordinate_tuple[0]
+                    ymin = coordinate_tuple[1]
+                    xmax = coordinate_tuple[2]
+                    ymax = coordinate_tuple[3]
+                    class_label = single_anno.split(':')[1].strip(',')
+                    single_instance.append(imgs_tuple[0])
+                    single_instance.append(imgs_tuple[1])
+                    single_instance.append(anno_key)
+                    # May append endcoded_image_data
+                    single_instance.append(xmin)
+                    single_instance.append(xmax)
+                    single_instance.append(ymin)
+                    single_instance.append(ymax)
+                    single_instance.append('Person')
+                    single_instance.append(class_label)
+                    examples.append(single_instance)
+    return examples                
+
           
   # Process the annotation file
 def main(_):
   writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
   
-  examples = []
-  
   # TODO(user): Write code to read in your dataset to examples variable
+  examples = make_examples(r'F:\81-DataSets\BAHNHOF')
 
   for example in examples:
     tf_example = create_tf_example(example)
